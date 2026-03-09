35a04417cef2473091b4cdbbf257479d76d4176c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-10 00:38:04 +0100

    more tests

--- maybe.scm
+++ maybe.scm
@@ -38,3 +38,11 @@
 (assert-eq (just 3)    (list 'just 3))
 (assert-eq (just "67") (list 'just "67"))
 (assert-eq (nothing)   (list 'nothing))
+
+(assert-eq (just? (just 3))    #t)
+(assert-eq (just? (just "67")) #t)
+(assert-eq (just? (nothing))   #f)
+
+(assert-eq (nothing? (just 3))    #f)
+(assert-eq (nothing? (just "67")) #f)
+(assert-eq (nothing? (nothing))   #t)
