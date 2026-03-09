3ad76eecddb40f90ac8f7fb84f9baa410b4761f2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-09 23:14:02 +0100

    naive non tail recursive list len

--- lists.scm
+++ lists.scm
@@ -7,6 +7,12 @@
         ((null? (cdr my-list)) (car my-list))
         (else                  (last (cdr my-list)))))
 
+(define (len my-list)
+  (if (null? my-list)
+      0
+      (+ 1 (len (cdr my-list)))))
+
 ;;; "mutators"
+
 (define (push-front element my-list)
   (cons element my-list))
