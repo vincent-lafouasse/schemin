20fcedf46e4df2025886925b4b3c3a352fd3919d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-10 05:32:58 +0100

    implement tests for maybe/map

--- maybe/maybe-tests.scm
+++ maybe/maybe-tests.scm
@@ -45,4 +45,12 @@
 (assert-eq (maybe/nothing? (maybe/just "67")) #f)
 (assert-eq (maybe/nothing? (maybe/nothing))   #t)
 
+(assert-eq (maybe/map (maybe/just 5)  (lambda (x) (* x 2))) (maybe/just 10))
+(assert-eq (maybe/map (maybe/just 3)  number->string)       (maybe/just "3"))
+(assert-eq (maybe/map (maybe/nothing) (lambda (x) (* x 2))) (maybe/nothing))
+(assert-eq (maybe/map (maybe/just -4) abs)                  (maybe/just 4))
+
+(let ((chained (maybe/map (maybe/map (maybe/just 3) (lambda (x) (* x 2))) (lambda (x) (+ x 1)))))
+  (assert-eq chained (maybe/just 7)))
+
 (display "-- everything ok\n")
