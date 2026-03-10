82e2cf4ab3a99de07f7f26b774a51568ef116ff8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-10 20:57:38 +0100

    extract assert "module", just a macro really

---
+++ assert.scm
@@ -0,0 +1,32 @@
+; do not come at me for the non standard formatting i do not care
+(define-syntax assert-eq
+  (syntax-rules () ; following is as many forms as use patterns we recognize. we only recognize 1
+    (
+      ; define the use pattern
+      ; we want 2 arguments. bind them to these names
+      (_ actual-expr expected-expr)
+
+      ; when encountering the macro, replace with:
+      (
+       ; evaluate the expressions once and use them in `let`'s second argument
+        let ((expected-value expected-expr) (actual-value actual-expr))
+
+        (if (not (equal? expected-value actual-value))
+          (begin
+            (display "-- assertion failed")
+            (newline)
+            (display "--   expected: ")
+            (display 'expected-expr) ; the quote means: do not evaluate, just print the S-expression
+            (display " => ")
+            (display expected-value) ; this is the value
+            (newline)
+            (display "--   actual:   ")
+            (display 'actual-expr)
+            (display " => ")
+            (display actual-value)
+            (newline)
+            ; exiting is fine, this is meant for batch mode not interactive mode
+            (exit 1)
+        ))
+      )
+    )))
