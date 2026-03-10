e95703208a641642819dfc901ae6b959c0d0ac4e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-10 22:07:58 +0100

    namespace colors in color/ e.g. color/red

--- color.scm
+++ color.scm
@@ -5,8 +5,6 @@
       (string-append "\033[" color "m" str "\033[0m")
       str))
 
-(define red    "31")
-(define green  "32")
-(define yellow "33")
-
-(display (colorize green "✓ all tests passed"))
+(define color/red    "31")
+(define color/green  "32")
+(define color/yellow "33")
