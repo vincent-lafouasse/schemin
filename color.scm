2c65b6001f999d711aa39873902d939b90d166c1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-10 22:06:58 +0100

    implement ANSI colorizer

--- color.scm
+++ color.scm
@@ -1,3 +1,12 @@
 (define (isatty?) (not (eq? (output-port-terminal-mode (current-output-port)) 'false)))
 
-(isatty?)
+(define (colorize color str)
+  (if (isatty?)
+      (string-append "\033[" color "m" str "\033[0m")
+      str))
+
+(define red    "31")
+(define green  "32")
+(define yellow "33")
+
+(display (colorize green "✓ all tests passed"))
