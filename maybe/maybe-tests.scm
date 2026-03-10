1b0dbd098ec9f3ff418032a17c54cad9edc05571
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-03-10 02:44:12 +0100

    sketch assert-eq macro

--- maybe/maybe-tests.scm
+++ maybe/maybe-tests.scm
@@ -4,6 +4,23 @@
   (if (not (equal? expected actual))
       (error "assertion failed: expected " expected 'but 'got actual)))
 
+; do not come at me for the non standard formatting i do not care
+(define-syntax assert-eq
+  (syntax-rules ()
+    ( ; the first and only use pattern recognized
+      (_ expected-expr actual-expr) ; we want 2 arguments. bind them to these names
+
+      ; when encountering the macro, replace with:
+      (
+       ; evaluate the expressions once and use them in `let`'s second argument
+        let ((expected-value expected-expr) (actual-value actual-expr))
+
+        (if (not (equal? expected-value actual-value))
+            (error "something use both 'expr and value")
+        )
+      )
+    )))
+
 (assert-eq (just 3)    (list 'just 3))
 (assert-eq (just "67") (list 'just "67"))
 (assert-eq (nothing)   (list 'nothing))
